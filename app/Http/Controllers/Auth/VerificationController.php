f4ed6804764ca547b9ab107577ddabed5e27beeb
GuitarCYQ <1159543426@qq.com>
2020-10-13 06:52:10 +0000

    认证后的消息提醒

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo ='/';
 
     /**
      * Create a new controller instance.
@@ -35,8 +35,11 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
+        //设定了所有的控制器动作都需要登录后才能访问
         $this->middleware('auth');
+        //设定了只有verify动作使用signed中间件进行认证
         $this->middleware('signed')->only('verify');
+        //对verify和resend 动作做了频率限制 throttle限制了一分钟不能超过6次
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
