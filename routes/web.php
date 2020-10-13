76b43cf6c6ad836e9eea9d06bc21f10fa710cebb
GuitarCYQ <1159543426@qq.com>
2020-10-13 14:36:15 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@ Route::post('email/resend','Auth\VerificationController@resend')->name('verifica
 
 //个人页面
 Route::resource('users','UsersController',['only' => ['show','update','edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
