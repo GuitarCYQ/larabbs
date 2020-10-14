4d725d7bccb87d491158e51188feeafb784bf720
GuitarCYQ <1159543426@qq.com>
2020-10-14 08:18:25 +0000

    “用户话题列表”

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,4 +28,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //用户与话题时一对多关系 一个用户拥有多个主题
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
