bb13a7163c2a7f879db6b42ab020b508d6130257
GuitarCYQ <1159543426@qq.com>
2020-10-14 07:29:31 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,13 @@
 <?php
 
+//获取路由名称 例如www.larabbs.com/users/1 得到的就是users-show
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
+}
+
+//判断路由 添加active
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category',$category_id)));
 }
--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -8,6 +8,7 @@
                     </a>
                 </div>
 
+
                 <div class="media-body">
 
                     <div class="media-heading mt-0 mb-1">
@@ -21,7 +22,7 @@
 
                     <small class="media-body meta text-secondary">
 
-                        <a class="text-secondary" href="#" title="{{ $topic->category->name }}">
+                        <a class="text-secondary" href="{{ route('categories.show',$topic->category_id) }}" title="{{ $topic->category->name }}">
                             <i class="far fa-folder"></i>
                             {{ $topic->category->name }}
                         </a>
