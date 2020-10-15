502801784f843326c7704cfbcf61e033edfce837
GuitarCYQ <1159543426@qq.com>
2020-10-15 13:07:31 +0000

    修复XSS注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,6 +21,9 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        //过滤XSS内容
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         //调用辅助函数里的make_excerpt
         $topic->excerpt = make_excerpt($topic->body);
     }
