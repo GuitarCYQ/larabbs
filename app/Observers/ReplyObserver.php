e91fd96afc4b967b4e61e903fde1ef7fb00a401e
GuitarCYQ <1159543426@qq.com>
2020-10-16 07:37:07 +0000

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,13 +9,19 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+
     public function creating(Reply $reply)
     {
-        //
+        //过滤XSS 使用的过滤规则时user_topic_body
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
-    public function updating(Reply $reply)
+    public function created(Reply $reply)
     {
-        //
+        //回复创建成功后 计算本话题下有多少回复
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
+
+
 }
